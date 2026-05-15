0dba8243ae1e2653f2d304d713f161c387664aaa
Audreysilverio <audrey_silverio@yahoo.com.br>
2026-05-14 23:04:23 -0300

    Correção

--- src/app.js
+++ src/app.js
@@ -1,22 +1,12 @@
-require('dotenv').config()
-
 const express = require('express')
 const cors = require('cors')
-const mongoose = require('mongoose')
-
-const routes = require('./routes')
 
 const app = express()
 
 app.use(cors({
-  origin: '*'
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
 }))
+
 app.use(express.json())
-
-app.use('/api', routes)
-
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log('Mongo conectado'))
-  .catch(err => console.log(err))
-
-module.exports = app
